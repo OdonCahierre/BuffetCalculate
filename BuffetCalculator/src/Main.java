32bd5590a799fcec331f860271467a24a6c61a07
OdonCahierre <ocahierre18@windwardschool.org>
2017-05-15 08:55:44 -0700

    Update Main.java

--- BuffetCalculator/src/Main.java
+++ BuffetCalculator/src/Main.java
@@ -6,12 +6,16 @@ public class Main {
 		int cost = z.nextInt();
 		double count[] = Storage();
 		double moneylost = Calculation(count, cost);
+		if(moneylost < 0){
+			System.out.println("You saved $" + Math.abs(moneylost));
+		}else{
 		System.out.println("You lost $" + moneylost);
 	}
+	}	
 	public static double[] Storage(){
 		Scanner s = new Scanner(System.in);
 		double[] amount = new double[10];
-		String[] list = {"Bacon", "Eggs", "3 oz of Potatoes", "Sausage", "Toast", "Pancakes", "Waffles", "Coffee", "Pizza", "Cereal"};
+		String[] list = {"Bacon", "Eggs", "3 oz of Potatoes", "Sausage", "Toast", "Pancakes/Waffles", "Cinnamon Roll", "Coffee", "Pizza", "Cereal"};
 		for(int i = 0; i < list.length; i++){
 			System.out.println("How many pieces/servings of " + list[i] + " did you have?");
 			int Pieces = s.nextInt();
@@ -21,7 +25,7 @@ public class Main {
 	}
 	
 	public static double Calculation(double[] x, int y){
-		double[] costPiece = {0.17, 0.15, 0.12, 4, 5, 6, 7, 8, 9, 10};
+		double[] costPiece = {0.17, 0.15, 0.12, 0.2, 0.035, 0.05, 0.3, 0.03, 9, 10};
 		double total = 0;
 		for(int i = 0; i < 10; i++){
 		total += costPiece[i]*x[i];	
